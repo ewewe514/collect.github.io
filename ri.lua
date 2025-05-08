3a0741c6735c7b08bff1bcb8a5123a62cff788cc
ewewe514 <eeeiqjjj221@gmail.com>
2025-05-08 17:23:58 -0400

    Update ri.lua

--- ri.lua
+++ ri.lua
@@ -1,19 +1,21 @@
-while true do
-    task.wait(0.3)
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local hrp = character:WaitForChild("HumanoidRootPart")
 
-    local player = game.Players.LocalPlayer
-    local char = player.Character or player.CharacterAdded:Wait()
-    local hrp = char:WaitForChild("HumanoidRootPart")
+task.spawn(function()
+    while true do
+        task.wait(0.3)
 
-    local items = game.Workspace:WaitForChild("RuntimeItems")
+        local items = game.Workspace:WaitForChild("RuntimeItems")
 
-    for _, bond in pairs(items:GetChildren()) do
-        if bond:IsA("Model") and bond.Name == "Bond" and bond.PrimaryPart then
-            local dist = (bond.PrimaryPart.Position - hrp.Position).Magnitude
-            if dist < 100 then
-                local rem = game.ReplicatedStorage.Packages.RemotePromise.Remotes.C_ActivateObject
-                rem:FireServer(bond)
+        for _, bond in pairs(items:GetChildren()) do
+            if bond:IsA("Model") and bond.Name == "Bond" and bond.PrimaryPart then
+                local dist = (bond.PrimaryPart.Position - hrp.Position).Magnitude
+                if dist < 100 then
+                    local rem = game.ReplicatedStorage.Packages.RemotePromise.Remotes.C_ActivateObject
+                    rem:FireServer(bond)
+                end
             end
         end
     end
-end
+end)
