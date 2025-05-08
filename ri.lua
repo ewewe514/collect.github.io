32b0b9743b3378f53592cfd2e5291ab180faf24a
ewewe514 <eeeiqjjj221@gmail.com>
2025-05-08 17:41:11 -0400

    Update ri.lua

--- ri.lua
+++ ri.lua
@@ -1,19 +1,16 @@
 task.spawn(function()
     while true do
-        if bond then
-            local activateRemote = game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("RemotePromise"):WaitForChild("Remotes"):FindFirstChild("C_ActivateObject")
+        local activateRemote = game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("RemotePromise"):WaitForChild("Remotes"):FindFirstChild("C_ActivateObject")
 
-            if activateRemote then
-                local runtimeItems = game.Workspace:WaitForChild("RuntimeItems")
+        if activateRemote then
+            local runtimeItems = game.Workspace:WaitForChild("RuntimeItems")
 
-                for _, bond in pairs(runtimeItems:GetChildren()) do
-                    if bond:IsA("Model") and (bond.Name == "Bond" or bond.Name == "Bonds") and bond.PrimaryPart then
-                        activateRemote:FireServer(bond)
-                        task.wait(0.3) -- This is the delay you're looking for
-                    end
+            for _, bond in pairs(runtimeItems:GetChildren()) do
+                if bond:IsA("Model") and (bond.Name == "Bond" or bond.Name == "Bonds") and bond.PrimaryPart then
+                    activateRemote:FireServer(bond)
                 end
             end
         end
-        task.wait()
+        task.wait(0.3) -- Adds the delay after each loop iteration
     end
 end)
