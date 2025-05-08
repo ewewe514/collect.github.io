865de16857008e72fb471f105499f9fbfea04f90
ewewe514 <eeeiqjjj221@gmail.com>
2025-05-08 17:45:11 -0400

    Update ri.lua

--- ri.lua
+++ ri.lua
@@ -1,16 +1,21 @@
 task.spawn(function()
     while true do
-        local activateRemote = game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("RemotePromise"):WaitForChild("Remotes"):FindFirstChild("C_ActivateObject")
+        local packages = game.ReplicatedStorage:FindFirstChild("Packages")
+        if packages then
+            local remotePromise = packages:FindFirstChild("RemotePromise")
+            if remotePromise then
+                local activateRemote = remotePromise:FindFirstChild("Remotes"):FindFirstChild("C_ActivateObject")
+                if activateRemote then
+                    local runtimeItems = game.Workspace:WaitForChild("RuntimeItems")
 
-        if activateRemote then
-            local runtimeItems = game.Workspace:WaitForChild("RuntimeItems")
-
-            for _, bond in pairs(runtimeItems:GetChildren()) do
-                if bond:IsA("Model") and (bond.Name == "Bond" or bond.Name == "Bonds") and bond.PrimaryPart then
-                    activateRemote:FireServer(bond)
+                    for _, bond in pairs(runtimeItems:GetChildren()) do
+                        if bond:IsA("Model") and (bond.Name == "Bond" or bond.Name == "Bonds") and bond.PrimaryPart then
+                            activateRemote:FireServer(bond)
+                        end
+                    end
                 end
             end
         end
-        task.wait(0.3) -- Adds the delay after each loop iteration
+        task.wait(0.3) -- Adds a delay between iterations
     end
 end)
