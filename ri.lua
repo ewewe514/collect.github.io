5ccd5a0110c25c1b33fe8edba1bb6e3808c9e0d1
ewewe514 <eeeiqjjj221@gmail.com>
2025-05-08 17:34:36 -0400

    Update ri.lua

--- ri.lua
+++ ri.lua
@@ -1,21 +1,19 @@
-local player = game.Players.LocalPlayer
-local character = player.Character or player.CharacterAdded:Wait()
-local hrp = character:WaitForChild("HumanoidRootPart")
-
 task.spawn(function()
     while true do
-        task.wait(0.3)
+        if bond then
+            local activateRemote = game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("RemotePromise"):WaitForChild("Remotes"):FindFirstChild("C_ActivateObject")
 
-        local items = game.Workspace:WaitForChild("RuntimeItems")
+            if activateRemote then
+                local runtimeItems = game.Workspace:WaitForChild("RuntimeItems")
 
-        for _, bond in pairs(items:GetChildren()) do
-            if bond:IsA("Model") and bond.Name == "Bond" and bond.PrimaryPart then
-                local dist = (bond.PrimaryPart.Position - hrp.Position).Magnitude
-                if dist < 100 then
-                    local rem = game.ReplicatedStorage.Packages.RemotePromise.Remotes.C_ActivateObject
-                    rem:FireServer(bond)
+                for _, bond in pairs(runtimeItems:GetChildren()) do
+                    if bond:IsA("Model") and (bond.Name == "Bond" or bond.Name == "Bonds") and bond.PrimaryPart then
+                        activateRemote:FireServer(bond)
+                        task.wait(0.3) -- This is the delay you're looking for
+                    end
                 end
             end
         end
+        task.wait()
     end
 end)
